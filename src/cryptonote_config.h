037b49ddf88749d50f2c133ed60b9f27928752e8
Alberto Esquitino <alberto.esqui2002@gmail.com>
2018-07-12 19:26:57 +0200

    Update cryptonote_config.h

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -159,7 +159,7 @@ namespace config
   boost::uuids::uuid const NETWORK_ID = { {
       0x22 ,0x30, 0xF5, 0x71 , 0x68, 0x14 , 0x81, 0x51, 0x15, 0x51, 0x20, 0x80, 0x06, 0x51, 0x81, 0x50
     } }; // Bender's nightmare
-  std::string const GENESIS_TX = "010a01ff0001ffffffffffff07029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210101f1652b40849d2fb400759d26dc202a6395892c84e1a3ea0cf2246b1e59077f";
+  std::string const GENESIS_TX = "013c01ff0001ffffffffffff07029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210101f1652b40849d2fb400759d26dc202a6395892c84e1a3ea0cf2246b1e59077f";
   uint32_t const GENESIS_NONCE = 10000;
 
   namespace testnet
@@ -173,7 +173,7 @@ namespace config
     boost::uuids::uuid const NETWORK_ID = { {
         0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x11
       } }; // Bender's daydream
-    std::string const GENESIS_TX = "010a01ff0001ffffffffffff07029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210101f1652b40849d2fb400759d26dc202a6395892c84e1a3ea0cf2246b1e59077f";
+    std::string const GENESIS_TX = "013c01ff0001ffffffffffff07029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210101f1652b40849d2fb400759d26dc202a6395892c84e1a3ea0cf2246b1e59077f";
     uint32_t const GENESIS_NONCE = 10001;
   }
 
